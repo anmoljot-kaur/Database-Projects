5af0c2ed09e561ca49824ffe7c5fd8706dbde5c1
Anmoljot Kaur <127451556+anmoljot-kaur@users.noreply.github.com>
2023-07-24 14:51:49 -0400

    Update and rename SELECT statement to SELECT statement.sql

---
+++ SELECT statement.sql
@@ -0,0 +1,15 @@
+/* Author: Anmoljot Kaur 
+ Date: July 21, 2023
+ Topic: Selecting Data from Database
+ Objectives:
+   1. Use the SELECT statement to query a database
+   2. Use the COUNT() function
+   3. Use the following operators to query a database:
+       <
+       >
+       =
+       WHERE
+       ORDER BY
+       AND  */
+
+
